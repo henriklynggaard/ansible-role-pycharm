062c297067082076a76cd3383c5729ba004e259b
Henrik Lynggaard Hansen <henrik@hlyh.dk>
2017-05-21 20:29:42 +0200

    Refactor and add tests

---
+++ tests/test_pycharm.py
@@ -0,0 +1,34 @@
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    '.molecule/ansible_inventory').get_hosts('all')
+
+
+desktop_file_location = "/root/.local/share/applications/pycharm-community-2017.1.1.desktop"
+
+
+def test_desktop_file_exists(File):
+    f = File(desktop_file_location)
+
+    assert f.exists
+    assert f.is_file
+
+
+def test_desktop_file_contains_fullpath(File):
+    f = File(desktop_file_location)
+
+    assert f.contains("/root/Tools/pycharm-community-2017.1.1/bin/pycharm.png")
+    assert f.contains("/root/Tools/pycharm-community-2017.1.1/bin/pycharm.sh")
+
+
+def test_desktop_file_contains_right_name(File):
+    f = File(desktop_file_location)
+
+    assert f.contains("PyCharm Community Edition 2017.1.1")
+
+
+def test_start_file_exists(File):
+    f = File('/root/Tools/pycharm-community-2017.1.1/bin/pycharm.sh')
+
+    assert f.exists
+    assert f.is_file
